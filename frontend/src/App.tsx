ec5afdba43c9a6d558e6f3e21a203beab555cfc9
Hitalo-27 <56124084+Hitalo-27@users.noreply.github.com>
2021-09-06 18:50:08 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Hello World</h1>
+    <>
+      <NavBar/>
+      <div className="container">
+        <h1 className="text-primary">Olá Mundo</h1>
+        <DataTable/>
+      </div>
+
+      <Footer/>
      
-    </div>
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return(
+        <div>
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https://github.com/Hitalo-27" target="_blank" rel="noreferrer">Hitalo Chaves</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
