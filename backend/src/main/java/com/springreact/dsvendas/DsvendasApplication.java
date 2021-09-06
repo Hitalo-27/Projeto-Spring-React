2dd72c8218635c01a55a64036fb7aaae5521a3f7
Hitalo-27 <56124084+Hitalo-27@users.noreply.github.com>
2021-09-06 17:57:04 -0300

    Project created

---
+++ backend/src/main/java/com/springreact/dsvendas/DsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.springreact.dsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/springreact/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springreact.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
