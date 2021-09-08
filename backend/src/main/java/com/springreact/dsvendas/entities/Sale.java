a4e5a68637b0adfb88ab3e137770fc707a0d0be7
Hitalo-27 <56124084+Hitalo-27@users.noreply.github.com>
2021-09-08 14:20:13 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/springreact/dsvendas/entities/Sale.java
@@ -0,0 +1,82 @@
+package com.springreact.dsvendas.entities;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private Seller seller;
+
+    public Sale(){
+
+    }
+
+    public Sale(Long id, Integer visited, Integer deals, Double amount, LocalDate date, Seller seller) {
+        this.id = id;
+        this.visited = visited;
+        this.deals = deals;
+        this.amount = amount;
+        this.date = date;
+        this.seller = seller;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getVisited() {
+        return visited;
+    }
+
+    public void setVisited(Integer visited) {
+        this.visited = visited;
+    }
+
+    public Integer getDeals() {
+        return deals;
+    }
+
+    public void setDeals(Integer deals) {
+        this.deals = deals;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Seller getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Seller seller) {
+        this.seller = seller;
+    }
+}
